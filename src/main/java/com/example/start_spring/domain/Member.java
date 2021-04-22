b0f9c6f551930931c06ee5b5fb9f7637f514f21a
Daein <dnfwlxo11@naver.com>
2021-04-22 22:33:40 +0900

    add: first commit, mvc pattern exam

---
+++ src/main/java/com/example/start_spring/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.start_spring.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}

b73f49c400e0d7a66b6f436b8a781e2c589a0c46
daein <dnfwlxo11@naver.com>
2021-04-23 13:56:35 +0900

    add: jdbc template

--- src/main/java/com/example/start_spring/SpringConfig.java
+++ src/main/java/com/example/start_spring/SpringConfig.java
@@ -1,13 +1,23 @@
 package com.example.start_spring;
 
+import com.example.start_spring.repository.JdbcTemplateMemberRepository;
 import com.example.start_spring.repository.MemberRepository;
-import com.example.start_spring.repository.MemoryMemberRepository;
 import com.example.start_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +25,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
